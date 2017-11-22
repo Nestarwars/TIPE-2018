4ac8eb02b8b13d102c3921cefd690aa2ff547fc7
Nestarwars <33899435+Nestarwars@users.noreply.github.com>
2017-11-22 14:25:35 +0100

    Update lexer.mli

--- Analyse lexicale/lexer.mli
+++ Analyse lexicale/lexer.mli
@@ -1,1 +1,25 @@
-type
+(**************************************************************************)
+(*                       TIPE 2018 - Compilation                          *)
+(*               Kineider    Bourret-Mathieu    Laborier                  *)
+(*                                                                        *)
+(*                          ANALYSE LEXICALE                              *)
+(*                                                                        *)
+(*                  This file is under WTFPL License                      *)
+(**************************************************************************)
+
+
+(* type des lexèmes reconnus, ici on se limite à l'arithmétique basique *)
+type lexeme =
+  | PLUS
+  | MOINS
+  | FOIS
+  | DIV
+  | eof
+  | NUM of int
+  | VAR of string
+;;
+
+type lexbuf = lexeme list ;;
+
+(* analyse_lex exécute l'analyse lexicale de la chaine en entrée, et la rend dans un lexbuf *)
+value analyse_lex : string -> lexbuf ;;
